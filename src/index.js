437611115f07724eb28f7ae41c7fa83c84fce713
Dan Mulvihillgit config --global user.name danMulvihillgit config --global user.name Dan <dmulvihill3@gmail.com>
2020-01-13 13:14:57 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var user ={
+    //name: "Dan",
+    location: "Philly",
+    options: [5,6,4,76]
+}
+const getLocation = (loc) => {
+    // if (loc){
+    //     return loc
+    // }else{
+    //     return "unknown"
+    // }
+    return loc ? loc : "unknown"
+}
+
+var template = <div>
+        <h1>{user.name?user.name.toUpperCase():"unknown"}</h1>
+        <h2>{getLocation(user.location)}</h2>
+        <p>{user.options.length>0?user.options.length+" ops": "No options!"}</p>
+    </div>;
+
+ReactDOM.render(template, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
