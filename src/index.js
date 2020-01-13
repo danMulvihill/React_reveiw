4cbe8fe43388ec21c81a2f5e90802a81f64eb936
Dan Mulvihillgit config --global user.name danMulvihillgit config --global user.name Dan <dmulvihill3@gmail.com>
2020-01-13 14:43:58 -0500

    added counter

--- src/index.js
+++ src/index.js
@@ -18,13 +18,43 @@ const getLocation = (loc) => {
     return loc ? loc : "unknown"
 }
 
-var template = <div>
+
+//counter
+let count = 0;
+const addOne = () => {
+    count++;
+    renderApp();
+}
+const minusOne = () => {
+    count--;
+    renderApp();
+}
+const reset = () => {
+    count = 0;
+    renderApp();
+}
+
+
+
+function renderApp(){
+     let template = <div>
         <h1>{user.name?user.name.toUpperCase():"unknown"}</h1>
         <h2>{getLocation(user.location)}</h2>
         <p>{user.options.length>0?user.options.length+" ops": "No options!"}</p>
+         <h1>Count: {count}</h1>
+        <button onClick={addOne}>+1</button>
+        <button onClick={minusOne}>-1</button>
+        <button onClick={reset}>reset</button>
     </div>;
 
-ReactDOM.render(template, document.getElementById('root'));
+    ReactDOM.render(template, document.getElementById('root'));
+
+}
+
+
+
+
+renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
